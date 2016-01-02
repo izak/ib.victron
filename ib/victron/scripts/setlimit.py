fd814e2b7af996c79be8aced9608eae46b720b72
Izak Burger <isburger@gmail.com>
2016-01-02 13:39:21 +0200

    make sure limit is set

--- ib/victron/scripts/setlimit.py
+++ ib/victron/scripts/setlimit.py
@@ -1,3 +1,4 @@
+import sys
 from serial import Serial
 from ib.victron.mk2 import MK2
 from ib.victron.scripts.options import parser, options
@@ -5,6 +6,10 @@ from ib.victron.scripts.options import parser, options
 def main():
     parser.add_argument("-l", "--limit", action="store", type=float,
         default=None, help="PowerAssist limit in Ampere")
+
+    if options.limit is None:
+        parser.print_help()
+        sys.exit(1)
 
     port = Serial(options.port, options.baudrate)
     mk2 = MK2(port)
