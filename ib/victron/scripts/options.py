90bb4cccb18fc3e6a0e008b205905f0e8b3a6ff8
Izak Burger <isburger@gmail.com>
2016-01-02 13:25:57 +0200

    Changes so we can modify the parser with extra options

--- ib/victron/scripts/options.py
+++ ib/victron/scripts/options.py
@@ -1,23 +1,27 @@
 import sys
 from argparse import ArgumentParser
 
-def _options():
-    parser = ArgumentParser(description=sys.argv[0])
-    parser.add_argument("-p", "--port", action="store", type=str,
+def _parser():
+    p = ArgumentParser(description=sys.argv[0])
+    p.add_argument("-p", "--port", action="store", type=str,
         default='/dev/ttyUSB0', help="Serial port for the MK2")
-    parser.add_argument("-b", "--baudrate", action="store", type=int,
+    p.add_argument("-b", "--baudrate", action="store", type=int,
         default=2400,
         help="Baud rate for the MK2, usually 2400, which is also the default")
-    return parser.parse_args()
+    return p
+parser = _parser()
 
 class Options(object):
-    def __init__(self, c):
-        self.callback = c
+    def __init__(self, p):
+        self.parser = p
         self.options = None
 
     def __getattr__(self, name):
         if self.options is None:
-            self.options = self.callback()
-        return getattr(self.options, name)
+            self.options = self.parser.parse_args()
+        try:
+            return getattr(self.options, name)
+        except AttributeError:
+            raise AttributeError(name)
 
-options = Options(_options)
+options = Options(parser)
